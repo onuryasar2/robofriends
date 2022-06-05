3048bf2a6b57b0af14009daaf3b31315b3455ecd
onuryasar2 <103512969+onuryasar2@users.noreply.github.com>
2022-06-06 02:27:33 +0300

    Create React App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React,{Component} from 'react';
+import CardList from './CardList';
+import SearchBox from './SearchBox'
+import { robots } from './robots';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            robots: robots,
+            searchfield: ''
+        }
+    }
+    render() {
+        return (
+            <div className='tc'>
+            <h1>RoboFriends</h1>
+            <SearchBox />
+            <CardList robots={this.state.robots}/>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/SearchBox.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div>
+        <input
+         className='pa3 ba b--green bg-lightest-blue'
+         type='search'
+         id=''
+         placeholder='Search Robots'
+        />
+        </div>
+    );
+}
+
+export default SearchBox;
